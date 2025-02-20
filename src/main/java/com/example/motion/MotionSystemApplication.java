e88dacc6c890f4f9c32d36eeeb1862c6ec0e0046
Martin Becke <Martin.Becke@HAW-Hamburg.de>
2025-02-20 15:47:52 +0000

    Add Spring Boot Application class

---
+++ src/main/java/com/example/motion/MotionSystemApplication.java
@@ -0,0 +1,11 @@
+package com.example.motion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotionSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MotionSystemApplication.class, args);
+    }
+}
