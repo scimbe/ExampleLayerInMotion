38f254696110f6afba995a4f6d504ecb7384f909
Martin Becke <Martin.Becke@HAW-Hamburg.de>
2025-02-20 10:52:17 +0100

    Update setup-java-env.sh

--- setup-java-env.sh
+++ setup-java-env.sh
@@ -7,8 +7,12 @@ if [ -z "$JAVA_PATH" ]; then
     exit 1
 fi
 
-# Resolve symbolic links to get real path
-JAVA_REAL_PATH=$(readlink -f "$JAVA_PATH")
+# Resolve symbolic links to get real path (macOS compatible)
+if [[ "$(uname)" == "Darwin" ]]; then
+    JAVA_REAL_PATH=$(perl -MCwd -e 'print Cwd::abs_path shift' "$JAVA_PATH")
+else
+    JAVA_REAL_PATH=$(readlink -f "$JAVA_PATH")
+fi
 
 # Set JAVA_HOME
 export JAVA_HOME=$(dirname $(dirname "$JAVA_REAL_PATH"))
