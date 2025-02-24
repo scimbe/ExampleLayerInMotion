5b43f4bb3155b2a25c5276c51a51a385da54d529
Martin Becke <Martin.Becke@HAW-Hamburg.de>
2025-02-24 21:29:37 +0000

    Entferne @Service-Annotation aus CharacterMotionServiceImpl

--- src/main/java/com/example/motion/services/CharacterMotionServiceImpl.java
+++ src/main/java/com/example/motion/services/CharacterMotionServiceImpl.java
@@ -6,13 +6,11 @@ import com.example.motion.sys.behavior.IMotionLayer;
 import com.example.motion.sys.data.IMotionDataRepository;
 import com.example.motion.sys.model.*;
 
-import org.springframework.stereotype.Service;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.stream.Collectors;
 
-@Service
 public class CharacterMotionServiceImpl implements ICharacterMotionService {
 
     private final IMotionDataRepository repository;
